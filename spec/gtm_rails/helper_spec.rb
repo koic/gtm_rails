95746eeda2714ec9923b58a846a027129d43599e
Koichi ITO <koic.ito@gmail.com>
2017-03-28 17:27:07 +0900

    Cosmetic

--- spec/gtm_rails/helper_spec.rb
+++ spec/gtm_rails/helper_spec.rb
@@ -7,46 +7,52 @@ describe GtmRails::Helper, type: :helper do
     GtmRails::Hooks.init
   end
 
-  let(:gtm) { 'GTM-XXXX' }
+  let(:container_id) { 'GTM-XXXX' }
 
   describe '#google_tag_manager_tag' do
     subject { helper.google_tag_manager(:foo) }
 
-    it { is_expected.to eq <<-EOS.strip_heredoc }
-      <!-- Google Tag Manager -->
-      <noscript><iframe src="//www.googletagmanager.com/ns.html?id=#{gtm}"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','#{gtm}');</script>
-      <!-- End Google Tag Manager -->
-    EOS
+    context 'container id is set' do
+      it { is_expected.to eq <<-EOS.strip_heredoc }
+        <!-- Google Tag Manager -->
+        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=#{container_id}"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','#{container_id}');</script>
+        <!-- End Google Tag Manager -->
+      EOS
+    end
   end
 
   describe '#google_tag_manager_script_tag' do
     subject { helper.google_tag_manager_script_tag(:foo) }
 
-    it { is_expected.to eq <<-EOS.strip_heredoc }
-      <!-- Google Tag Manager -->
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','#{gtm}');</script>
-      <!-- End Google Tag Manager -->
-    EOS
+    context 'container id is set' do
+      it { is_expected.to eq <<-EOS.strip_heredoc }
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','#{container_id}');</script>
+        <!-- End Google Tag Manager -->
+      EOS
+    end
   end
 
   describe '#google_tag_manager_noscript_tag' do
     subject { helper.google_tag_manager_noscript_tag(:foo) }
 
-    it { is_expected.to eq <<-EOS.strip_heredoc }
-      <!-- Google Tag Manager (noscript) -->
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=#{gtm}"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-      <!-- End Google Tag Manager (noscript) -->
-    EOS
+    context 'container id is set' do
+      it { is_expected.to eq <<-EOS.strip_heredoc }
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=#{container_id}"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+      EOS
+    end
   end
 end
